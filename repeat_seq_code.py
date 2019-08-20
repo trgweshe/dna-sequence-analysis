02446bfc80742a640f6d41cfc2473be0be40fe8a
trgweshe <32386157+trgweshe@users.noreply.github.com>
2019-08-20 11:18:09 +0200

    Update repeat_seq_code.py

--- repeat_seq_code.py
+++ repeat_seq_code.py
@@ -1,4 +1,4 @@
-
+#after searching for a solution, i think using regular expressions would be the way forwars but i am open to ideas and i would appreciate all the help i can get
 
 
 new_records= open("dna.example.fasta").read()
